0a6de00663c9618fee27e8b483cb2a6096923c53
Barbara Lima <Babiplima123>
2023-06-19 18:01:27 -0300

    first commit

---
+++ slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Barbara Lima on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
